da5e0bd8dabe58876df6c580bdae3f312adef623
bma <beiji.ma@outlook.com>
2021-12-12 03:30:12 +0100

    basic setup including nacos config

---
+++ anyware-ware/src/main/java/org/corpname/anymall/ware/AnymallWareApplication.java
@@ -0,0 +1,13 @@
+package org.corpname.anymall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AnymallWareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnymallWareApplication.class, args);
+    }
+}
---
+++ anyware-ware/src/main/java/org/corpname/anymall/ware/controller/InventoryController.java
@@ -0,0 +1,7 @@
+package org.corpname.anymall.ware.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryController {
+}
